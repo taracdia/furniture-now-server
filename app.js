65a4d54faa982e842b5b4d820bbe161da00f8f61
Tara Dia <taracdia@gmail.com>
2020-12-03 19:55:51 -0600

    easy authentication

--- app.js
+++ app.js
@@ -41,6 +41,31 @@ app.use(function(err, req, res, next) {
   res.render("error");
 });
 
+function auth(req, res, next){
+  console.log(req.headers);
+  const authHeader = req.headers.authorization;
+  if (!authHeader){
+    const err = new Error("You are not authenticated!");
+    res.setHeader("WWW-Authenticate", "Basic");
+    err.status = 401;
+    return next(err);
+  }
+
+  const auth = Buffer.from(authHeader.split(" ")[1], "base64").toString().split(":");
+  const user = auth[0];
+  const pass = auth[1];
+  if (user === "admin" && pass === "password"){
+    return next();
+  } else {
+    const err = new Error("You are not authenticated");
+    res.setHeader("WWW-Authenticate", "Basic");
+    err.status = 401;
+    return next(err)
+  }
+}
+
+app.use(auth)
+
 const mongoose = require("mongoose");
 const url = "mongodb://localhost:27017/furniture-now";
 const connect = mongoose.connect(url, {
